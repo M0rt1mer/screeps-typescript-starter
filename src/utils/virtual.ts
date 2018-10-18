576a0c87e700f479636858a5a39e544dece8cf30
M0rt1mer <M0rt1mer@users.noreply.github.com>
2018-10-18 13:40:27 +0200

    Improved virtualization, added inheritable static Construct method

--- src/utils/virtual.ts
+++ src/utils/virtual.ts
@@ -1,23 +1,34 @@
-import { inspect } from "util";
-
 let VirtualRegistry: { [id: string]: any } = {};
+
+interface __Constructor<T> { 
+    new(...args: any[]): T;
+}
 
 export class VirtualClass {
   // identifier of instance
   classId: string;
-  //fake???
+  //fake, does nothing, but won't compile without this
   public readonly __proto__: any;
 
   constructor() {
     this.classId = this.__proto__.constructor.name;
   }
 
-  Do() { console.log("tst"); }
-  
   public static RegisterVirtualClass() : void {
     VirtualRegistry[this.name] = this.prototype;
   }
  
+  public static Construct<T extends VirtualClass>(this: __Constructor<T>, instance: any): T { 
+    if (!instance.classId) {
+      return undefined;
+    }
+    var instanceType = VirtualRegistry[instance.classId];
+    if (!(instanceType instanceof this) && instanceType.constructor != this) {
+      return undefined; 
+    }
+    Object.setPrototypeOf(instance, VirtualRegistry[instance.classId]);
+      return <T>instance;
+  }
 }
 
 export function Construct_cast<T extends VirtualClass>(instance: any): T | undefined {
@@ -28,7 +39,19 @@ export function Construct_cast<T extends VirtualClass>(instance: any): T | undef
   return <T>instance;
 }
 
-export function IsTypeOf<T extends VirtualClass>(instance: any, ctor: { new(...args: any[]): T }): boolean {
+function Checked_construct_cast<T extends VirtualClass>(instance: any, ctor: __Constructor<T>) : T { 
+  if (!instance.classId) {
+    return undefined;
+    }
+    var instanceType = VirtualRegistry[instance.classId]; 
+    if (!(instanceType instanceof ctor) && instanceType.constructor != ctor) {
+        return undefined; 
+    }
+    Object.setPrototypeOf(instance, VirtualRegistry[instance.classId]);
+    return <T>instance;
+}
+
+export function IsTypeOf<T extends VirtualClass>(instance: any, ctor: __Constructor<T>): boolean {
   if (!instance.classId) {
     return false;
   }
